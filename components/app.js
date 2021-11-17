40f283eb5b006328dfe4e53639f2cd11b9b2272c
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-11-17 10:10:51 +0000

    feat: add algolia and renders basic results from url params

--- components/app.js
+++ components/app.js
@@ -1,29 +1,92 @@
 import React from 'react'
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, connectSearchBox, Hits, Configure, connectHits } from 'react-instantsearch-dom';
+// import styles from './App.module.css'
 
 const App = () => {
-  // const router = useRouter()
+
+  const searchClient = algoliasearch(
+    '853MYZ81KY',
+    '1bc06bbf6de499f6b826a8a0e6902568'
+  );
+
+  const router = useRouter()
+
+  console.log(router.query, router.asPath)
+
+  let searchTerms = router.query
+
+  // const showSearchTerms = (searchTerms) => {
+    for (let [key,value] of Object.entries(searchTerms)) {
+      console.log(key, value);
+    };
+  // }
+
+  const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => {
+    // refine is a function. Look at the docs
+    refine(`${router.query.product}`);
+    // refine('shirt');
+    // return null means render no html to the page, but we maintain the functionality
+    return null;
+  };
+  
+  const CustomSearchBox = connectSearchBox(SearchBox);
+
+  const Hits = ({ hits }) => {
+    console.log(hits);
+    return (
+    <ul>
+      {hits.map(hit => (
+        <li key={hit.objectID}>
+          <img src={hit.full_url_image} alt='no-img'style={{height: '150px'}}/>
+          {hit.name}
+          {hit.price}
+          </li>
+      ))}
+    </ul>
+  )};
+  
+  const CustomHits = connectHits(Hits);
+
 
   return (
+
+    <InstantSearch searchClient={searchClient} indexName="flagship_fashion">
+      {/* Most of the received params will be a filter. Unusable filters at this time are: 'collar', 'fulfillment', and 'fit' */}
+    <Configure 
+      filters='genderFilter:men AND colour:white AND sizeFilter:S'
+      hitsPerPage={4} 
+    />
     <div id="container" className="container">
       <div
         id="buy-together"
         // className={styles.buytogether}
-      ></div>
+      >'TODO:bought together</div>
+      <CustomSearchBox />
+
+      <CustomHits />
+      {/* We want to take the received query params and plug them into a search query that runs when we load the page (useEffect?)  */}
       <div
         id="main-product"
         //className={styles.mainproduct}
-      ></div>
+      >
+        YOU WANT TO BUY A:
+        {router.query.product}
+      </div>
       <div
         id="upsell-product"
         //className={styles.upsellproduct}
-      ></div>
+      >TODO: upsell product</div>
       <div
         id="promo-code"
         // className={styles.promocode}
-      ></div>
+      >TODO: promo code</div>
     </div>
+
+    </InstantSearch>
   )
 }
 
 export default App
+
