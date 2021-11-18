3d145846cbd35bae5a9e31cd2c483c7cd5d6d05b
Matthew Foyle <mfoyle93@gmail.com>
2021-11-18 19:14:31 +0000

    fix(app): load accessories after main product

--- components/app.js
+++ components/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import algoliasearch from 'algoliasearch/lite'
 import {
@@ -8,27 +8,38 @@ import {
   Configure,
   connectHits,
   Index,
-  StateResults,
+  connectStateResults,
 } from 'react-instantsearch-dom'
 
 import appStyles from './App.module.css'
 
 const App = () => {
+  const [accessoriesCategory, setAccessoriesCategory] = useState('null')
+  const StateResults = ({ allSearchResults }) => {
+    if (
+      allSearchResults &&
+      allSearchResults.product &&
+      allSearchResults.product.hits.length
+    ) {
+      if (
+        accessoriesCategory !== allSearchResults.product.hits[0].accessories
+      ) {
+        setAccessoriesCategory(allSearchResults.product.hits[0].accessories)
+      }
+    }
+    return null
+  }
+
+  const CustomStateResults = connectStateResults(StateResults)
+
   const searchClient = algoliasearch(
     '853MYZ81KY',
     '1bc06bbf6de499f6b826a8a0e6902568'
   )
   const router = useRouter()
-  console.log(router.query, router.asPath)
-  let searchTerms = router.query
-  // const showSearchTerms = (searchTerms) => {
-  for (let [key, value] of Object.entries(searchTerms)) {
-    console.log(key, value)
-  }
-  // }
 
   const SearchBox = ({ currentRefinement, isSearchStalled, refine, query }) => {
-    console.log(query)
+    console.log('query', query)
     // refine is a function. Look at the docs
     refine(query)
     // refine('shirt');
@@ -40,11 +51,10 @@ const App = () => {
   const CustomSearchBoxProductAccessories = connectSearchBox(SearchBox)
 
   const Hits = ({ hits }) => {
-    console.log(hits)
     return (
       <div className={appStyles.carouselContainer}>
         {hits.map((hit) => (
-          <div key={hit.objectID}>
+          <div className={appStyles.productContainer} key={hit.objectID}>
             <img src={hit.image} alt="no-img" style={{ height: '150px' }} />
             {hit.name}
             {hit.price}
@@ -60,7 +70,7 @@ const App = () => {
   return (
     <InstantSearch searchClient={searchClient} indexName="chatbot-demo">
       {/* Most of the received params will be a filter. Unusable filters at this time are: 'collar', 'fulfillment', and 'fit' */}
-
+      <CustomStateResults />
       <div id="container" className={appStyles.container}>
         <Index indexName="chatbot-demo" indexId="product">
           <Configure
@@ -79,41 +89,21 @@ const App = () => {
             <p>Buy together and save X%</p>
             <p>Button to buy together</p>
           </div>
-          <CustomSearchBoxProduct query={router.query.product} />
+          <CustomSearchBoxProduct query="shirt" />
           <CustomHitsProduct />
           {/* We want to take the received query params and plug them into a search query that runs when we load the page (useEffect?) */}
-          <div id="main-product" className={appStyles.mainproduct}>
-            CAROUSEL WITH THREE
-            {router.query.product}
-          </div>
         </Index>
         <div id="upsell-product" className={appStyles.upsellproduct}>
           <Index indexName="chatbot-demo" indexId="accessories">
             <Configure
               // filters="genderFilter:men AND colour:white AND sizeFilter:S"
-              hitsPerPage={4}
+              hitsPerPage={3}
+              filters={`category:${accessoriesCategory}`}
             />
-            <div id="buy-together" className={appStyles.buytogether}>
-              <div>
-                <p>Shirt</p>
-                <p>buy it button</p>
-              </div>
-              <div>
-                <p>Bowtie</p>
-                <p>buy it button</p>
-              </div>
-              <p>Buy together and save X%</p>
-              <p>Button to buy together</p>
-            </div>
-            <CustomSearchBoxProductAccessories query="bowtie" />
+            <CustomSearchBoxProductAccessories query="" />
             <CustomHitsAccessories />
             {/* We want to take the received query params and plug them into a search query that runs when we load the page (useEffect?) */}
-            <div id="main-product" className={appStyles.mainproduct}>
-              CAROUSEL WITH THREE
-              {router.query.product}
-            </div>
           </Index>
-          TODO: CAROUSEL WITH THREE ACCESSORIES
         </div>
       </div>
     </InstantSearch>
